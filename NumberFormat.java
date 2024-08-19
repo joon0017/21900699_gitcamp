29d34d0ea806081605e83fb4bec186a41ef01056
joon0017 <21900699@handong.ac.kr>
2024-08-19 14:09:35 +0900

    Number Formatter added
    created a java class for formatting numbers

--- NumberFormat.java
+++ NumberFormat.java
@@ -5,30 +5,77 @@ public class NumberFormat {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int input = 0;
+        // long input = 0;
+        String input = new String();
 
         
         System.out.println("양의 정수를 입력하세요: ");
         int c= 0;
         while(c < 3){
-            try{
-                input = Integer.parseInt(scan.nextLine());
-                break;
-            }
-            catch(Exception e){
-                System.out.println("양의 정수를 입력하세요: ");
+                input = scan.nextLine();
+                if(check(input)){
+                    break;
+                };
+                System.out.println("잘못된 입력입니다. 다시 입력하세요: ");
                 c++;
-            }
         }
-
+        
         printNumber(input);
-
-
-
-        System.out.println("입력한 값은 " + input + "입니다.");
     }
 
-    public static void printNumber(int input){
-        
+    public static boolean check(String input){
+        if(input.length()<=20 ){
+
+            try{
+                Long.parseLong(input);
+                return true;
+            } catch(Exception e){
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static void printNumber(long input){
+        String output = Long.toString(input);
+
+        System.out.print("입력한 값은 ");
+        int length = output.length();
+        int check = length%3;
+
+        for (int i = 0; i < output.length(); i++) {
+            
+            if(check == i && i!=0){
+                System.out.print(","); 
+            }
+            else if ((i - check) % 3 == 0 && i != 0){
+                System.out.print(",");
+            }
+            System.out.print(output.charAt(i));
+        }
+
+        System.out.print("입니다.");
+
+    }
+
+    public static void printNumber(String input){
+
+        System.out.print("입력한 값은 ");
+        // int length = input.length();
+        int check = input.length()%3;
+
+        for (int i = 0; i < input.length(); i++) {
+            
+            if(check == i && i!=0){
+                System.out.print(","); 
+            }
+            else if ((i - check) % 3 == 0 && i != 0){
+                System.out.print(",");
+            }
+            System.out.print(input.charAt(i));
+        }
+
+        System.out.print("입니다.");
+
     }
 }
